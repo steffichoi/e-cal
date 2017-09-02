e1b8889a9127fc67da147c24dea4c3a0401b9cd6
steffichoi <steffic.06@gmail.com>
2017-09-01 23:15:01 -0400

    updated file for sidebar date change functionality

--- app/src/main/java/com/example/android/me_cal/Fragments/CustomCalendarView.java
+++ app/src/main/java/com/example/android/me_cal/Fragments/CustomCalendarView.java
@@ -1,8 +1,7 @@
 package com.example.android.me_cal.Fragments;
 
 import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
+import android.icu.text.DateFormatSymbols;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
@@ -28,14 +27,25 @@ public class CustomCalendarView extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
         myView = inflater.inflate(R.layout.custom_calendar_layout, container, false);
         calendarView = (CalendarView) myView.findViewById(R.id.calendarView);
-        date = calendarView.getDate();
 
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
-                helperFunctions.switchMainContentFragment(new TodayFragment(), getActivity());
+
+                Fragment newSideBarFragment = new TodaySideBarFragment();
+                Bundle bundle = new Bundle();
+
+                String monthFromCal = new DateFormatSymbols().getMonths()[month];;
+                String dateFromCal = Integer.toString(dayOfMonth);
+
+                bundle.putString("date_from_cal", dateFromCal);
+                bundle.putString("month_from_cal", monthFromCal);
+                newSideBarFragment.setArguments(bundle);
+
+                helperFunctions.switchSideContentFragment(newSideBarFragment, getActivity());
 
             }
         });
