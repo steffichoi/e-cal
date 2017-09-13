5b8758d9323583f0ce6856e95def004966410f26
steffichoi <steffic.06@gmail.com>
2017-09-13 18:40:55 -0400

    pushing task detail fragment; works with updated db

--- app/src/main/java/com/example/android/me_cal/Fragments/TaskDetailFragment.java
+++ app/src/main/java/com/example/android/me_cal/Fragments/TaskDetailFragment.java
@@ -12,12 +12,15 @@ import android.support.design.widget.FloatingActionButton;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.android.me_cal.Helper.HelperFunctionsFragment;
 import com.example.android.me_cal.R;
 import com.example.android.me_cal.data.AddTaskContract;
 import com.example.android.me_cal.data.AddTaskDbHelper;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by steffichoi on 8/18/17.
@@ -36,54 +39,60 @@ public class TaskDetailFragment extends Fragment {
         myView = inflater.inflate(R.layout.task_detail_layout, container, false);
 
         Bundle bundle = getArguments();
-        String taskName = "";
+        long taskTime = -1;
+
         if (bundle != null) {
-            taskName= bundle.getString("task_name", "no task");
+            taskTime = bundle.getLong("task_time");
         }
 
         AddTaskDbHelper dbHelper = new AddTaskDbHelper(getActivity());
         mDb = dbHelper.getWritableDatabase();
 
-        Cursor cursor = dbHelper.getTask(taskName);
+        Cursor cursor = dbHelper.getTask(taskTime);
 
-        TextView taskMonthTv = (TextView) myView.findViewById(R.id.detail_month_tv);
-        TextView taskDateTv = (TextView) myView.findViewById(R.id.detail_date_tv);
         TextView taskNameTv = (TextView) myView.findViewById(R.id.detail_item_name_tv);
-        TextView taskTimeTv = (TextView) myView.findViewById(R.id.detail_item_time_tv);
-        TextView taskDurationTv = (TextView) myView.findViewById(R.id.detail_item_duration_tv);
+        TextView taskStartDateTv = (TextView) myView.findViewById(R.id.detail_item_start_date_tv);
+        TextView taskStartTimeTv = (TextView) myView.findViewById(R.id.detail_item_start_time_tv);
+        TextView taskEndDateTv = (TextView) myView.findViewById(R.id.detail_item_end_date_tv);
+        TextView taskEndTimeTv = (TextView) myView.findViewById(R.id.detail_item_end_time_tv);
         TextView taskLocationTv = (TextView) myView.findViewById(R.id.detail_item_location_tv);
-        TextView taskTypeTv = (TextView) myView.findViewById(R.id.detail_item_type_tv);
         TextView taskReminderTv = (TextView) myView.findViewById(R.id.detail_item_reminder_tv);
-
-        String monthDateFromDb = cursor.getString(cursor.getColumnIndex(AddTaskContract.AddTaskEntry.COLUMN_TASK_DATE));
-        String dateDb = monthDateFromDb.split("/")[0];
-        int monthDb = Integer.parseInt(monthDateFromDb.split("/")[1])-1;
-        String monthFin = new DateFormatSymbols().getMonths()[monthDb];;
-
+        TextView taskTypeTv = (TextView) myView.findViewById(R.id.detail_item_type_tv);
 
         String nameFromDb = cursor.getString(cursor.getColumnIndex(AddTaskContract.AddTaskEntry.COLUMN_TASK_NAME));
-        String timeFromDb = cursor.getString(cursor.getColumnIndex(AddTaskContract.AddTaskEntry.COLUMN_TASK_TIME));
-        String durationFromDb = cursor.getString(cursor.getColumnIndex(AddTaskContract.AddTaskEntry.COLUMN_TASK_DURATION));
         String locationFromDb = cursor.getString(cursor.getColumnIndex(AddTaskContract.AddTaskEntry.COLUMN_TASK_LOCATION));
-//        String typeFromDb = cursor.getString(cursor.getColumnIndex(AddTaskContract.AddTaskEntry.COLUMN_TASK_TYPE));
-//        String reminderFromDb = cursor.getString(cursor.getColumnIndex(AddTaskContract.AddTaskEntry.COLUMN_TASK_REMINDER));
+        String typeFromDb = cursor.getString(cursor.getColumnIndex(AddTaskContract.AddTaskEntry.COLUMN_TASK_TYPE));
+
+        long startTimeFromDb = cursor.getLong(cursor.getColumnIndex(AddTaskContract.AddTaskEntry.COLUMN_TASK_TIME_START));
+        long endTimeFromDb = cursor.getLong(cursor.getColumnIndex(AddTaskContract.AddTaskEntry.COLUMN_TASK_TIME_END));
+
+        String[] start = helperFunctions.getDateTime(startTimeFromDb).split("\\s+");
+        String[] end = helperFunctions.getDateTime(endTimeFromDb).split("\\s+");
 
         taskNameTv.setText(nameFromDb);
-        taskMonthTv.setText(monthFin);
-        taskDateTv.setText(dateDb);
-        taskTimeTv.setText("Start   \t\t\t: " + timeFromDb);
-        taskDurationTv.setText("End   \t\t\t   : " + durationFromDb);
-        taskLocationTv.setText("Location \t: " + locationFromDb);
-        taskTypeTv.setText("Type   \t\t\t  : ");
-        taskReminderTv.setText("Reminder \t: ");
+        taskStartDateTv.setText(start[0] + " " + start[1] + " " + start[2]);
+        taskStartTimeTv.setText(start[3]);
+        taskEndDateTv.setText(end[0] + " " + end[1] + " " + end[2]);
+        taskEndTimeTv.setText(end[3]);
+        taskLocationTv.setText(locationFromDb);
+        taskTypeTv.setText(typeFromDb);
 
-        FloatingActionButton fab = (FloatingActionButton) myView.findViewById(R.id.detail_edit_fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        String reminderFromDb = cursor.getString(cursor.getColumnIndex(AddTaskContract.AddTaskEntry.COLUMN_TASK_REMINDER));
+
+        if (reminderFromDb.equals("Custom")) {
+            long reminderCustomFromDb = cursor.getLong(cursor.getColumnIndex(AddTaskContract.AddTaskEntry.COLUMN_TASK_CUSTOM_REMINDER));
+            taskReminderTv.setText(helperFunctions.getDateTime(reminderCustomFromDb));
+        }
+
+
+
+
+        ImageView editButton= (ImageView) myView.findViewById(R.id.detail_item_edit_button);
+        editButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 helperFunctions.switchMainContentFragment(new AddTaskFragment(), getActivity());
                 helperFunctions.switchSideContentFragment(new TodaySideBarFragment(), getActivity());
-
             }
         });
 
